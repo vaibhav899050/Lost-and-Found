ee043048ada9576b319f1ae09c1f698cdb82d496
Vaibhav <vg899050@gmail.com>
2022-10-13 23:42:34 +0530

    added basic functions

--- app/src/main/java/com/example/lostandfound/additem.kt
+++ app/src/main/java/com/example/lostandfound/additem.kt
@@ -2,10 +2,29 @@ package com.example.lostandfound
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.EditText
+import androidx.core.view.isVisible
+
 
 class additem : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_additem)
+        val lostbtn = findViewById<Button>(R.id.lostbtn)
+        val foundbtn = findViewById<Button>(R.id.foundbtn)
+        val headtxt = findViewById<TextView>(R.id.headtxt)
+        lostbtn.setOnClickListener {
+            headtxt.setText("Item Name")
+            lostbtn.isVisible =false
+            foundbtn.isVisible = false
+        }
+        foundbtn.setOnClickListener {
+            headtxt.setText("Item Name")
+            lostbtn.isVisible =false
+            foundbtn.isVisible = false
+        }
+
     }
 }
