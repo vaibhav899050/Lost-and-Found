72b62a1245dd13b3c3bce184b4857ea7a974ac63
Vaibhav <vg899050@gmail.com>
2022-10-17 01:15:09 +0530

    added email and password authentication

--- app/src/main/java/com/example/lostandfound/MainActivity.kt
+++ app/src/main/java/com/example/lostandfound/MainActivity.kt
@@ -11,16 +11,24 @@ import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.core.view.isVisible
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthException
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.ktx.app
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var firebase: FirebaseAuth
+    private lateinit var reason: FirebaseAuthException
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val db = Firebase.firestore
+        firebase = Firebase.auth
+
 
         val text1 = findViewById<EditText>(R.id.ul)
         val text2 = findViewById<EditText>(R.id.pl)
@@ -73,37 +81,58 @@ class MainActivity : AppCompatActivity() {
             if(ur.text.toString().trim().isNotEmpty() && pr.text.toString().trim().isNotEmpty() && re.text.toString().trim().isNotEmpty() && rnum.text.toString().trim().isNotEmpty()){
                 val users = hashMapOf("uesrname" to ur.text.toString(), "password" to pr.text.toString(), "email" to re.text.toString(), "phone" to rnum.text.toString())
 
-                userdb.get().addOnCompleteListener {
-                    if(it.isSuccessful){
-                        count = 0
-                        for(document in it.result){
-                            
-                            if(rnum.text.toString()==document.data.getValue("phone")){
-                                count+=1
-                            }
-                            
+//                userdb.get().addOnCompleteListener {
+//                    if(it.isSuccessful){
+//                        count = 0
+//                        for(document in it.result){
+//
+//                            if(rnum.text.toString()==document.data.getValue("phone")){
+//                                count+=1
+//                            }
+//
+//                        }
+//                    }
+//                }
+//                if(count!=0){
+//                    Toast.makeText(applicationContext, "Phone number is already registered", Toast.LENGTH_LONG).show()
+//                }
+//                if(count==0){
+//
+//
+//
+//
+//                }
+                firebase.createUserWithEmailAndPassword(re.text.toString(), pr.text.toString())
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+
+                            userdb.add(users)
+                            //Toast.makeText(applicationContext, "You are now registered", Toast.LENGTH_SHORT).show()
+                            text1.isVisible = true
+                            text2.isVisible = true
+                            lbtn.isVisible = true
+                            ur.isVisible = false
+                            pr.isVisible = false
+                            re.isVisible = false
+                            rnum.isVisible = false
+                            rbtn.isVisible = false
+                            rtxt.setTextColor(Color.DKGRAY)
+                            rtxt.setTextSize(22f)
+                            ltxt.setTextColor(Color.WHITE)
+                            ltxt.setTextSize(30f)
+                            // Sign in success, update UI with the signed-in user's information
+
+                            val user = firebase.currentUser
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            //val message = firebase.res
+
+                            Toast.makeText(baseContext, "Email is wrong or already in use",
+                                Toast.LENGTH_SHORT).show()
+
                         }
                     }
-                }
-                if(count!=0){
-                    Toast.makeText(applicationContext, "Phone number is already registered", Toast.LENGTH_LONG).show()
-                }
-                if(count==0){
-                    userdb.add(users)
-                    Toast.makeText(applicationContext, "You are now registered", Toast.LENGTH_SHORT).show()
-                    text1.isVisible = true
-                    text2.isVisible = true
-                    lbtn.isVisible = true
-                    ur.isVisible = false
-                    pr.isVisible = false
-                    re.isVisible = false
-                    rnum.isVisible = false
-                    rbtn.isVisible = false
-                    rtxt.setTextColor(Color.DKGRAY)
-                    rtxt.setTextSize(22f)
-                    ltxt.setTextColor(Color.WHITE)
-                    ltxt.setTextSize(30f)
-                }
 
             }
             else{
