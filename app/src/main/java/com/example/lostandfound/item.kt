1cd6fd3bddc3358c499a9df541b9c510c6aa72e1
Vaibhav <vg899050@gmail.com>
2022-10-17 14:44:44 +0530

    added fields

--- app/src/main/java/com/example/lostandfound/item.kt
+++ app/src/main/java/com/example/lostandfound/item.kt
@@ -1,3 +1,3 @@
 package com.example.lostandfound
 
-data class item(var item: String ?= null, var location: String ?=null, var date: String ?= null, var status: String ?= null)
+data class item(var item: String ?= null, var location: String ?=null, var date: String ?= null, var status: String ?= null, var downloadurl: String?=null, var roll: String?=null)
