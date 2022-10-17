305c47a480e563eef51e10043d4b0204d4685250
Vaibhav <vg899050@gmail.com>
2022-10-17 12:58:42 +0530

    added user data class

---
+++ app/src/main/java/com/example/lostandfound/usermodel.kt
@@ -0,0 +1,10 @@
+package com.example.lostandfound
+
+data class usermodel(
+    val name: String?=null,
+    val email: String?=null,
+    val password: String?=null,
+    val roll: String?=null,
+    val imageurl: String?=null,
+    val phone: String?=null
+)
