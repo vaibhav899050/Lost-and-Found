5e4bb28202d0d3b5e8f18d984be85438bbd139e7
Vaibhav <vg899050@gmail.com>
2022-10-17 14:46:43 +0530

    added item image

--- app/src/main/java/com/example/lostandfound/additem.kt
+++ app/src/main/java/com/example/lostandfound/additem.kt
@@ -1,17 +1,29 @@
 package com.example.lostandfound
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.*
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.view.isVisible
+import com.google.firebase.database.ktx.database
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.ktx.storage
 
 
 class additem : AppCompatActivity() {
+
+    private lateinit var uri: Uri
+    private lateinit var storage: FirebaseStorage
     override fun onCreate(savedInstanceState: Bundle?) {
+
+
         super.onCreate(savedInstanceState)
+        storage = Firebase.storage
         setContentView(R.layout.activity_additem)
         val db = Firebase.firestore
         val lostbtn = findViewById<Button>(R.id.lostbtn)
@@ -25,9 +37,30 @@ class additem : AppCompatActivity() {
         val desc = findViewById<EditText>(R.id.descedt)
         val title = findViewById<EditText>(R.id.itemname)
         val roll = findViewById<EditText>(R.id.roll)
+        val aditmbtn = findViewById<Button>(R.id.additm)
+        val imagel = findViewById<LinearLayout>(R.id.imagel)
+        val itemimage = findViewById<ImageView>(R.id.itemimage)
+        val addimage = findViewById<Button>(R.id.addimage)
         val itemdb = db.collection("items")
         val intent = Intent(this , home::class.java)
         var status = ""
+        val gallery = registerForActivityResult(ActivityResultContracts.GetContent(),
+            ActivityResultCallback {
+                itemimage.setImageURI(it)
+                aditmbtn.isVisible = true
+                uri = it
+
+            })
+
+
+        val storageref = storage.reference
+
+
+        addimage.setOnClickListener {
+            gallery.launch("image/*")
+        }
+
+
         lostbtn.setOnClickListener {
             catl.isVisible = false
             forml.isVisible = true
@@ -40,11 +73,36 @@ class additem : AppCompatActivity() {
             status = "Found"
 
         }
+
         nextbtn.setOnClickListener {
+            forml.isVisible = false
+            imagel.isVisible = true
+        }
+
+
+
+        aditmbtn.setOnClickListener {
             if(location.text.toString().isNotEmpty() && date.text.toString().isNotEmpty() && title.text.toString().isNotEmpty() && desc.text.toString().isNotEmpty() && roll.text.toString().isNotEmpty()){
-                val item = hashMapOf("item" to title.text.toString(), "desc" to desc.text.toString(), "roll" to roll.text.toString(), "location" to location.text.toString(), "status" to status.toString(), "date" to date.text.toString())
-                itemdb.add(item)
-                Toast.makeText(applicationContext, "Item Added", Toast.LENGTH_SHORT).show()
+                //val item = hashMapOf("item" to title.text.toString(), "desc" to desc.text.toString(), "roll" to roll.text.toString(), "location" to location.text.toString(), "status" to status.toString(), "date" to date.text.toString())
+                //itemdb.add(item)
+
+
+                storageref.child(System.currentTimeMillis().toString()).putFile(uri).addOnSuccessListener { task->
+                    task.metadata!!.reference!!.downloadUrl
+                        .addOnSuccessListener {
+                            var downloadurl = it.toString()
+                            val item = hashMapOf("item" to title.text.toString(), "desc" to desc.text.toString(), "roll" to roll.text.toString(), "location" to location.text.toString(),
+                                "status" to status.toString(),
+                                "date" to date.text.toString(),
+                            "downloadurl" to downloadurl)
+
+                            itemdb.add(item)
+                            Toast.makeText(applicationContext, "Item Added", Toast.LENGTH_SHORT).show()
+
+                        }
+
+
+                }
 
 
             }
